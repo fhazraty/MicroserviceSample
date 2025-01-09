992bbafaafdb4e48a492ad93bd4392bd77b76773
Farhad <f.hazraty@gmail.com>
2025-01-09 10:25:02 +0330

    InitialCommit

---
+++ WebAppSample/WebAppSample/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppSample.Models;
+using WebAppSample.Services;
+
+namespace WebAppSample.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly ProductService _productService;
+
+		public ProductsController(ProductService productService)
+		{
+			_productService = productService;
+		}
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			return Ok(_productService.GetAll());
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			var product = _productService.GetById(id);
+			if (product == null)
+				return NotFound();
+			return Ok(product);
+		}
+
+		[HttpPost]
+		public IActionResult Add(Product product)
+		{
+			_productService.Add(product);
+			return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Update(int id, Product product)
+		{
+			if (id != product.Id)
+				return BadRequest();
+
+			_productService.Update(product);
+			return NoContent();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			_productService.Delete(id);
+			return NoContent();
+		}
+	}
+}
---
+++ WebAppSample/WebAppSample/Services/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using WebAppSample.Models;
+
+namespace WebAppSample.Services
+{
+	public class ProductService
+	{
+		private readonly List<Product> _products = new();
+
+		public ProductService()
+		{
+			// نمونه داده اولیه
+			_products.Add(new Product { Id = 1, Name = "Laptop", Price = 1000, Stock = 10 });
+			_products.Add(new Product { Id = 2, Name = "Mouse", Price = 20, Stock = 50 });
+		}
+
+		public IEnumerable<Product> GetAll() => _products;
+
+		public Product GetById(int id) => _products.FirstOrDefault(p => p.Id == id);
+
+		public void Add(Product product) => _products.Add(product);
+
+		public void Update(Product product)
+		{
+			var existingProduct = _products.FirstOrDefault(p => p.Id == product.Id);
+			if (existingProduct != null)
+			{
+				existingProduct.Name = product.Name;
+				existingProduct.Price = product.Price;
+				existingProduct.Stock = product.Stock;
+			}
+		}
+
+		public void Delete(int id)
+		{
+			var product = _products.FirstOrDefault(p => p.Id == id);
+			if (product != null)
+				_products.Remove(product);
+		}
+	}
+}
